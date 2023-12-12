5cf245367da4be51c4d722af1cac485f4d115935
rlaxNstdy <144894046+rlaxNstdy@users.noreply.github.com>
2023-12-12 21:48:46 +0700

    v2.0
    adding the  about us and fix things up with the home page

--- main.js
+++ main.js
@@ -3,13 +3,12 @@ showSlides();
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("image_slide");
+  let slides = document.getElementsByClassName("images-slide");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex % slides.length].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides,  6000); // Change image every 6 seconds
 }
-  
