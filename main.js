7ed590946001ed31ad7006fbcde1de8a7e8127b0
rlaxNstdy <144894046+rlaxNstdy@users.noreply.github.com>
2023-12-12 11:01:40 +0700

    v1.5

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ showSlides();
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName("image_slide");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 1000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
   
