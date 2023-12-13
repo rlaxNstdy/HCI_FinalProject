586cacf887c3104ebe18bf3f88723a7adfaa2565
rlaxNstdy <144894046+rlaxNstdy@users.noreply.github.com>
2023-12-13 21:14:41 +0700

    v.3.0
    adding the card fix some thing up and making the button login works

---
+++ property.js
@@ -0,0 +1,9 @@
+
+
+const /** {NodeList} */ $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach($toggleBtn => {
+    $toggleBtn.addEventListener("click", () => {
+        $toggleBtn.classList.toggle("active");
+    });
+});
