b499bd5c439d910024efe4229e23e0b3807ab439
rlaxNstdy <144894046+rlaxNstdy@users.noreply.github.com>
2023-12-13 11:04:10 +0700

    v2.4

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+// for the scroll button on the nav
+  document.addEventListener("DOMContentLoaded", function () {
+    // Select all links with the 'navbar' class
+    const navLinks = document.querySelectorAll('.navbar a');
+
+    // Add click event listener to each link
+    navLinks.forEach(link => {
+      link.addEventListener('click', function (e) {
+        e.preventDefault(); // Prevent default behavior of the link
+        const targetId = this.getAttribute('href').substring(1); // Get the target id from the href attribute
+        const targetElement = document.getElementById(targetId); // Get the target element by id
+
+        // Scroll smoothly to the target element
+        if (targetElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop - document.querySelector('.nav').offsetHeight, // Adjusted for fixed header
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+  });
+
+// for the slideshow 
 let slideIndex = 0;
 showSlides();
 
@@ -12,3 +36,6 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides,  6000); // Change image every 6 seconds
 }
+
+
+
